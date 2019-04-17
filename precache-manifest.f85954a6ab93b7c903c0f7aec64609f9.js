d17b4e39121b1f77a7d2c0ddbcef2da543693988
fobossalmeron <fobos.salmeron@gmail.com>
2019-04-17 21:03:09 +0200

    Updates

---
+++ precache-manifest.f85954a6ab93b7c903c0f7aec64609f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3227ee6768dc887e977",
+    "url": "/message/static/js/runtime~main.b20462c7.js"
+  },
+  {
+    "revision": "1a82e2d0258ac8d8b548",
+    "url": "/message/static/js/main.32f54889.chunk.js"
+  },
+  {
+    "revision": "a39a333903297ebbb5cd",
+    "url": "/message/static/js/2.dcf23f0b.chunk.js"
+  },
+  {
+    "revision": "1a82e2d0258ac8d8b548",
+    "url": "/message/static/css/main.6f5b73b2.chunk.css"
+  },
+  {
+    "revision": "6583062970791bbd9b3a627680093f56",
+    "url": "/message/index.html"
+  }
+];
